412c537b39e872b4ca1c03cb76fd86542a6a81b5
Kevin He <lawrence@kevins-mbp.home>
2020-09-27 02:29:23 +1000

    added matched view

---
+++ codebrew2020/src/views/Matched.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>
+            You have been match!
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Matched"
+}
+</script>
