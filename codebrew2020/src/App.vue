19f73f5bbb35c5ee159fa00bf5a56c341ab245b6
Kevin He <lawrence@kevins-mbp.home>
2020-09-26 17:34:40 +1000

    added vuetify theme

--- codebrew2020/src/App.vue
+++ codebrew2020/src/App.vue
@@ -1,18 +1,79 @@
 <template>
-  <div id='app'>
-    <navBar></navBar>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                @click="changeRouter(item)"
+              >
+                <v-list-item-icon>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+        <router-view/>
+        </v-row>
+      </v-container>
+      
+    </v-main>
+    
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import navBar from './components/navBar'
-export default {
-  name: "App",
-  components: {
-    navBar
+  export default {
+    name: 'App',
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      items: [
+          { title: 'Home', icon: 'mdi-home-city', path: '/home' },
+          { title: 'My Account', icon: 'mdi-account', path: '/userProfile' },
+          { title: 'Room', icon: 'mdi-account-group-outline', path: '/room' },
+        ]
+    }),
+    created () {
+      this.$vuetify.theme.dark = false
+    },
+    methods: {
+      changeRouter(item) {
+        this.$router.push(item.path)
+      }
+    }
   }
-}
 </script>
 
 <style>
